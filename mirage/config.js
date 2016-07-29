49c7e25a597b26bd296de88720ba86594d99e3ba
Stoyan Ekupov <ekupov@gmail.com>
2016-07-29 05:38:40 +0300

    Added more employees

--- mirage/config.js
+++ mirage/config.js
@@ -45,7 +45,68 @@ export default function() {
                     incometax: "3157.80",
                     nationalinsurance: "2199.48"
                 }
-        }];
+            }, {
+                type: 'employees',
+                id: 4,
+                attributes: {
+                    firstname: "Owen",
+                    surname: "Humphreys",
+                    gender: "male",
+                    title: "Mr.",
+                    dob: "15/05/1972",
+                    age: "43",
+                    salary: "31336.00",
+                    takehome: "24395.68",
+                    incometax: "4147.20",
+                    nationalinsurance: "2793.12"
+                }
+            }, {
+                type: 'employees',
+                id: 5,
+                attributes: {
+                    firstname: "Holly",
+                    surname: "Gregory",
+                    gender: "female",
+                    title: "Ms.",
+                    dob: "31/01/1993",
+                    age: "22",
+                    salary: "60176.00",
+                    takehome: "42227.78",
+                    incometax: "13473.40",
+                    nationalinsurance: "4474.82"
+                }
+            }, {
+                type: 'employees',
+                id: 6,
+                attributes: {
+                    firstname: "Skye",
+                    surname: "Lawrence",
+                    gender: "female",
+                    title: "Mrs.",
+                    dob: "22/06/1979",
+                    age: "36",
+                    salary: "42552.00",
+                    takehome: "32005.86",
+                    incometax: "6423.80",
+                    nationalinsurance: "4122.34"
+                }
+            }, {
+                type: 'employees',
+                id: 7,
+                attributes: {
+                    firstname: "Tom",
+                    surname: "Carey",
+                    gender: "male",
+                    title: "Mr.",
+                    dob: "03/06/1994",
+                    age: "21",
+                    salary: "75316.00",
+                    takehome: "51008.98",
+                    incometax: "19529.40",
+                    nationalinsurance: "4777.62"
+                }
+            }
+        ];
 
         if(request.queryParams.name !== undefined) {
             let filteredEmployees = employees.filter(function(i) {
